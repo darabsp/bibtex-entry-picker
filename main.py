33062666876b6f32c3b67b0672cebde99ebc3091
darabsp <51314762+darabsp@users.noreply.github.com>
2025-11-05 02:04:35 +0900

    [fix/add] 関数分離, README追記

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from formatting_style import Style
-from pybtex.database import parse_file
+from pybtex.database import parse_file, Entry
 import os
 import pyperclip
 import sys
+
+def search_entry(entries, key) -> Entry:
+  entry = entries.get(key)
+
+  if entry is None:
+    suggestions = list(filter(lambda k: key.lower() in k.lower(), entries.keys()))
+    if len(suggestions) == 1:
+      entry = entries.get(suggestions[0])
+    elif len(suggestions) > 1:
+      raise KeyError(f'エントリが一意に定まりません ({key}) (候補: {", ".join(suggestions)})')
+    else:
+      raise KeyError(f'エントリが見つかりません ({key})')
+
+  return entry
 
 def main(args) -> None:
   if len(args) < 3:
@@ -11,24 +25,14 @@ def main(args) -> None:
     raise ValueError(f'引数が多すぎます ({len(args)})')
 
   bibfile_path = args[1]
-  entry_key = args[2]
+  key = args[2]
 
   if not os.path.isfile(bibfile_path):
     raise FileNotFoundError(f'ファイルが見つかりません ({bibfile_path})')
 
   bibfile = open(bibfile_path)
   bib = parse_file(bibfile)
-  entry = bib.entries.get(entry_key)
-
-  if entry is None:
-    autocomplete = list(filter(lambda key: entry_key.lower() in key.lower(), bib.entries.keys()))
-    if len(autocomplete) == 1:
-      entry = bib.entries.get(autocomplete[0])
-    elif len(autocomplete) > 1:
-      raise KeyError(f'エントリが一意に定まりません ({entry_key}) (候補: {", ".join(autocomplete)})')
-    else:
-      raise KeyError(f'エントリが見つかりません ({entry_key})')
-
+  entry = search_entry(bib.entries, key)
   formatted = Style().format_entry(None, entry).text.render_as('text')
 
   pyperclip.copy(formatted)
