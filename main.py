d895aefeec2adbfefa44c7fb355aa1fab01d30e7
darabsp <51314762+darabsp@users.noreply.github.com>
2025-11-08 23:45:56 +0900

    [improve] 引数に関する処理をargparseを用いた処理に変更

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
+from argument_utils import parse_args
 from formatting_style import Style
 from search_utils import search_bib_file, search_entry
 from pybtex.database import parse_file
 import pyperclip
 import sys
 
-def main(args) -> None:
-  if len(args) < 3:
-    raise ValueError(f'引数が不足しています ({len(args)})')
-  if len(args) > 3:
-    raise ValueError(f'引数が多すぎます ({len(args)})')
+def main() -> None:
+  try:
+    args = parse_args()
+    path = search_bib_file(args.bib)
+    key = args.key
 
-  path = search_bib_file(args[1])
-  key = args[2]
+    bib = parse_file(path)
+    entry = search_entry(bib.entries, key)
+    formatted = Style().format_entry(None, entry).text.render_as('text')
 
-  bib = parse_file(path)
-  entry = search_entry(bib.entries, key)
-  formatted = Style().format_entry(None, entry).text.render_as('text')
-
-  pyperclip.copy(formatted)
-  print(f'コピーしました: {formatted}')
+    pyperclip.copy(formatted)
+    print(f'コピーしました: {formatted}')
+  except SystemExit as e:
+    sys.exit(e.code)
+  except BaseException as e:
+    sys.exit(f'エラーが発生しました ({e.__class__.__name__}): {" ".join(map(str, e.args))}')
 
 if __name__ == '__main__':
-  try:
-    main(args=sys.argv)
-  except BaseException as e:
-    sys.exit(f'エラーが発生しました ({e.__class__.__name__}): {" ".join(e.args)}')
+  main()
